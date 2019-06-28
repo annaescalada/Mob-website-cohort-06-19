3f5cae4b3eb4d2533ce9ba91684db7564735bf55
annaescalada <annaescalada@gmail.com>
2019-06-28 15:28:08 +0200

    Toggle in experiments

--- annaEmain.js
+++ annaEmain.js
@@ -1,3 +1,25 @@
 'use strict';
 
-var experiment = 
+
+
+function main() {
+  var experimentTitles = document.querySelectorAll('#my-experiments h3');
+  console.log(experimentTitles)
+  experimentTitles.forEach(function (title) {
+    title.addEventListener('click', showAndHide);
+  });
+
+  function showAndHide(event) {
+    console.log(event.target)
+    var title = event.target;
+    var header = title.parentNode;
+    console.log(header);
+    var content = header.nextSibling.nextSibling;
+    console.log(content);
+    content.classList.toggle('display-none')
+  }
+
+}
+
+window.addEventListener('load', main);
+
